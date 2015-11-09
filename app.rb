77d0376c5478eed54fa1397c79db01149a909646
slurmzmckenzie <brendanwilde@gmail.com>
2015-11-09 14:20:15 +0000

    added code to catch arguement error

--- app.rb
+++ app.rb
@@ -40,10 +40,16 @@ class HangpersonApp < Sinatra::Base
   post '/guess' do
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
+    begin
     @game.guess(letter)
     if @game.guesses.include? letter or @game.wrong_guesses.include? letter
       flash[:message] = "You have already used that letter."
     end
+    
+      rescue ArgumentError
+      flash[:message] = "Invalid guess."
+    end
+    
     redirect '/show'
   end
   
